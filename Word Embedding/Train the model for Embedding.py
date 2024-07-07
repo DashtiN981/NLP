a658a07fa1ce1c01e5c7d6f24028fe5396f08c23
DashtiN981 <naghme.dashti@gmail.com>
2024-07-07 22:43:29 +0330

    new code
    upload

--- Word Embedding/Train the model for Embedding.py
+++ Word Embedding/Train the model for Embedding.py
@@ -1,1 +1,13 @@
 
+import tensorflow as tf
+#print(tf.__version__)
+
+import nltk
+nltk.download('punkt')
+
+import pandas as pd
+import gensim
+from gensim.models import Word2Vec, KeyedVectors
+
+# 2) Data Processing
+
