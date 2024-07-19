f5e610f511202c3b50a2536354d3bc48a766bbde
DashtiN981 <naghme.dashti@gmail.com>
2024-07-19 11:13:08 +0330

    upload
    changes

---
+++ Text Classification with CNN/Spam Detection with CNN.py
@@ -0,0 +1,15 @@
+# 1) Data Preprocessing
+
+#import os
+#os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+import tensorflow as tf
+print(tf.__version__)
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from tensorflow
+
