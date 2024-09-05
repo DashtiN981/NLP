3836e6eafe3dfeffbcde2cae024c7e78ce0578a2
DashtiN981 <naghme.dashti@gmail.com>
2024-09-05 18:30:08 +0330

    update

--- Basics of NLP/Tokenization.py
+++ Basics of NLP/Tokenization.py
@@ -1,7 +1,10 @@
-import spacy
-nlp = spacy.load(name="en_core_web_md")
 s1 = "welome to NLP training"
+
+import spacy
+nlp = spacy.load(name='en_core_web_sm')
+
 doc1 = nlp(s1)
 for token in doc1:
     print(token)
+    
 print(len(doc1))
